a8717b03b99726fea9fdeaeccb5ff9bdb21e7f47
Evan Digby <evandigby@gmail.com>
2021-09-20 11:05:29 -0700

    simplify everything

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,8 +1,6 @@
 using BlazorFluentUI;
 using Client;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Net.Http;
@@ -10,24 +8,9 @@ using System.Net.Http;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 
-builder.Services.AddMsalAuthentication(options =>
-{
-    builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
-    options.ProviderOptions.DefaultAccessTokenScopes.Add("https://lunarcommand.onmicrosoft.com/console-log/Log.Read");
-    options.ProviderOptions.DefaultAccessTokenScopes.Add("openid");
-    options.ProviderOptions.DefaultAccessTokenScopes.Add("offline_access");
-    options.ProviderOptions.LoginMode = "redirect";
-});
+var baseAddress = builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress;
+builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(baseAddress) });
 
-builder.Services.AddScoped<CustomAuthorizationMessageHandler>();
-
-builder.Services
-    .AddHttpClient("Client.ServerAPI", client => client.BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress))
-    .AddHttpMessageHandler<CustomAuthorizationMessageHandler>();
-
-builder.Services.AddScoped(sp => new HttpClient { 
-    BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress),
-});
 builder.Services.AddBlazorFluentUI();
 
 await builder.Build().RunAsync();
