4b93b15eaa7859fea59bbe3086db343c038fd8b0
Evan Digby <evandigby@gmail.com>
2021-09-22 11:39:50 -0700

    need to add log folder

---
+++ Data/Log/LogEntry.cs
@@ -0,0 +1,21 @@
+﻿using Data.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Data.Log
+{
+
+    [JsonConverter(typeof(LogEntryConverter))]
+    public abstract class LogEntry
+    {
+        public Guid Id {  get; set; }
+        public Guid MissionId { get; set; }
+        public LogEntryType EntryType { get; set; }
+        public string LoggedBy { get; set; }
+        public DateTime LoggedAt { get; set; }
+    }
+}
