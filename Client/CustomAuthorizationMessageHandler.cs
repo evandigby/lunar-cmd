1670073ff23c885317e31c164b895b60463a2b87
Evan Digby <evandigby@gmail.com>
2021-09-24 21:02:07 -0700

    manual all the way up

---
+++ Client/CustomAuthorizationMessageHandler.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+
+namespace Client
+{
+    public class CustomAuthorizationMessageHandler : AuthorizationMessageHandler
+    {
+        public CustomAuthorizationMessageHandler(IAccessTokenProvider provider,
+            NavigationManager navigationManager,
+            State state)
+            : base(provider, navigationManager)
+        {
+            ConfigureHandler(
+                authorizedUrls: new[] { state.BaseAddress.ToString() },
+                scopes: new[] { "https://lunarcommand.onmicrosoft.com/cnc/LogEntries.Read", "https://lunarcommand.onmicrosoft.com/cnc/LogEntries.Write" });
+        }
+    }
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,7 +1,8 @@
-using AzureStaticWebApps.Blazor.Authentication;
 using BlazorFluentUI;
 using Client;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Net.Http;
@@ -10,9 +11,26 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 
 var baseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress);
-builder.Services.AddScoped(_ => new HttpClient { BaseAddress = baseAddress }).AddStaticWebAppsAuthentication();
+
 builder.Services.AddSingleton<State>(new State(baseAddress));
 builder.Services.AddBlazorFluentUI();
 
+builder.Services.AddMsalAuthentication(options =>
+{
+    builder.Configuration.Bind("AzureAdB2C", options.ProviderOptions.Authentication);
+    options.ProviderOptions.LoginMode = "redirect";
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("openid");
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("offline_access");
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("https://lunarcommand.onmicrosoft.com/cnc/LogEntries.Read");
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("https://lunarcommand.onmicrosoft.com/cnc/LogEntries.Write");
+});
+
+builder.Services.AddScoped<CustomAuthorizationMessageHandler>();
+
+builder.Services.AddHttpClient("WebAPI",
+        client => client.BaseAddress = baseAddress)
+    .AddHttpMessageHandler<CustomAuthorizationMessageHandler>();
+
+builder.Services.AddTransient(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("WebAPI"));
 
 await builder.Build().RunAsync();
