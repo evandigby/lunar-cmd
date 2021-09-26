ae69f528e22f919603117442bfed16b0a2b3843a
Evan Digby <evandigby@gmail.com>
2021-09-25 19:37:22 -0700

    exception

--- Api/LogEntriesService.cs
+++ Api/LogEntriesService.cs
@@ -39,15 +39,22 @@ namespace api
            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "lawgEntries")] HttpRequest req,
            ILogger log)
         {
-            var accessToken = GetAccessToken(req);
-            var claimsPrincipal = await ValidateAccessToken(accessToken, log);
-            if (claimsPrincipal != null)
+            try
             {
-                return (ActionResult)new OkObjectResult(claimsPrincipal.FindFirst("appid").Value);
+                var accessToken = GetAccessToken(req);
+                var claimsPrincipal = await ValidateAccessToken(accessToken, log);
+                if (claimsPrincipal != null)
+                {
+                    return (ActionResult)new OkObjectResult(claimsPrincipal.FindFirst("appid").Value);
+                }
+                else
+                {
+                    return (ActionResult)new OkObjectResult($"{accessToken} HI. It didn't work");
+                }
             }
-            else
+            catch (Exception ex)
             {
-                return (ActionResult)new OkObjectResult($"{accessToken} HI. It didn't work");
+                return (ActionResult)new OkObjectResult(ex);
             }
         }
 
@@ -75,9 +82,9 @@ namespace api
             TokenValidationParameters validationParameters = new()
             {
                 // App Id URI and AppId of this service application are both valid audiences.
-                ValidAudiences = new[] { "https://lunarcommand.onmicrosoft.com/cnc/LogEntries.Read", "https://lunarcommand.onmicrosoft.com/cnc/LogEntries.Write", clientID },
+                ValidAudiences = new[] { clientID },
                 // Support Azure AD V1 and V2 endpoints.
-                ValidIssuers = new[] { "https://login.microsoftonline.com/a4d31d01-f721-4605-8831-34490dc0b8f5/v2.0", "https://login.microsoftonline.com/lunarcommand.onmicrosoft.com/v2.0" },
+                ValidIssuers = new[] { "https://login.microsoftonline.com/a4d31d01-f721-4605-8831-34490dc0b8f5/v2.0" },
                 IssuerSigningKeys = config.SigningKeys
             };
             //try
